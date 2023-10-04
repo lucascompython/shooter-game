43c482dfae042842cd4aa3aa882e54d7dacbf370
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-10-04 15:19:04 +0100

    Set camera callback to Physics to remove jitter

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -10,17 +10,16 @@ var can_laser := true
 var can_grenade := true
 
 func _physics_process(_delta: float) -> void:
-	look_at(get_global_mouse_position())
-	
-	
 	# input
 	var direction := Input.get_vector("left", "right", "up", "down")
 	#position += direction * SPEED * delta
-	
+
 	velocity = direction * SPEED 
 	move_and_slide()
-	
+
 func _process(_delta: float) -> void:
+	look_at(get_global_mouse_position())
+	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
 		$LaserShootingParticles.emitting = true
